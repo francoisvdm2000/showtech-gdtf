87b4a82251bb56780e251cbf794ffefbf3f88db0
Nounours <nounours@Air-de-Nounours.localdomain>
2026-04-21 00:03:33 +0200

    Fix Railway PORT variable

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+# Railway injecte $PORT dynamiquement
+PORT=${PORT:-80}
+
+# Remplace le port dans nginx config
+sed -i "s/listen 80/listen $PORT/" /etc/nginx/sites-available/default
+
+# Démarre php-fpm puis nginx
 php-fpm -D
 nginx -g "daemon off;"
