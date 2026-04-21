337ea032ea15148fb00ebb905759f5791f884391
Nounours <nounours@Air-de-Nounours.localdomain>
2026-04-21 09:27:54 +0200

    fix: client_max_body_size 30M dans start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 PORT=${PORT:-80}
 
-# Écrit une config nginx fraîche avec le bon port
 cat > /etc/nginx/sites-available/default << NGINX
 server {
     listen $PORT;
     root /var/www/html;
     index index.php;
+
+    client_max_body_size 30M;
 
     location / {
         try_files \$uri \$uri/ =404;
@@ -16,6 +17,8 @@ server {
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         include fastcgi_params;
+        fastcgi_read_timeout 120;
+        fastcgi_send_timeout 120;
     }
 
     location ~ /\. {
